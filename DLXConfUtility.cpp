f58c0baf4d09981e4590326bca1248d2db6af1cd
oosamabsk <omar.osama@abrightskiesinc.com>
2023-02-26 11:40:01 +0200

    Initial commit

---
+++ DLXConfUtility.cpp
@@ -0,0 +1,11 @@
+#include <DLXConfUtility.h>
+
+DLXConfUtility::DLXConfUtility(KernelAPI * pKernelAPI): WriterBase(pKernelAPI),pKernelAPI(pKernelAPI){
+
+}
+
+DLXConfUtility::~DLXConfUtility(){
+
+}
+
+
---
+++ DLXConfUtility.h
@@ -0,0 +1,19 @@
+#include <WriterBase.h>
+#include <KernelAPI.h>
+
+#include <QtCore/QStringList>
+#include <QtDBus/QtDBus>
+#include <QDebug>
+#include <QThread>
+
+class DLXConfUtility : public WriterBase {
+public:
+    DLXConfUtility(KernelAPI * pKernelAPI);
+    virtual ~DLXConfUtility();
+    virtual std::vector<std::wstring> fScan() = 0;
+    virtual bool fConnect(std::wstring ssid=L"") = 0;
+    virtual void fConfigure(std::string ssh_cmd,std::string ip,std::string passowrd,std::string username) = 0;
+    virtual void fReset() = 0;
+protected:
+    KernelAPI * pKernelAPI;
+};
